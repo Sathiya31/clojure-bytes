82622fd86c4a09f9c7da0a33f04b8b3cb0b00761
Sathiya31 <kspriyan31@gmail.com>
2021-05-18 21:44:27 +0530

    Functions and Destructuring

---
+++ src/clojure_bytes/destructure.clj
@@ -0,0 +1,77 @@
+(ns clojure-bytes.destructure)
+
+(def my-line [[5 10] [10 20]])
+
+;; destructuring the two parameters and further destructuring each point
+(let [p1 (first my-line)
+      p2 (second my-line)
+      x1 (first p1)
+      y1 (second p1)
+      x2 (first p2)
+      y2 (second p2)]
+  (println "Line from (" x1 "," y1 ") to (" x2 ", " y2 ")"))
+
+(let [[p1 p2] my-line
+      [x1 y1] p1
+      [x2 y2] p2]
+  (println "Line from (" x1 "," y1 ") to (" x2 ", " y2 ")"))
+
+;; destructuring vector values
+(def my-vector [1 2 3])
+
+(let [[x y z] my-vector]
+  (println x y z))
+
+(let [[x y] my-vector]
+  (println x y))
+
+(let [[x & remaining] my-vector]
+  (println x remaining))
+
+(let [[x & remaining :as all] my-vector]
+  (println x remaining)
+  (println all))
+
+;; Associative destructuring
+
+(def client {:name "Super Co."
+             :location "Philadelphia"
+             :description "The worldwide leader in plastic tableware."})
+
+(let [name (:name client)
+      location (:location client)
+      description (:description client)]
+  (println name location "-" description))
+
+;; extract using keys
+(let [{:keys [name location description]} client]
+  (println name location "-" description))
+
+;; extract keys with a name
+(let [{company-name :name
+       location :location
+       description :description} client]
+  (println company-name location "-" description))
+
+(let [{category :category} client]
+  (println category))
+
+;; default values
+(let [{category :category, :or {category "Category not found"}} client]
+  (println category))
+
+
+;; multi level destructuring
+(def multiplayer-game-state
+  {:joe {:class "Ranger"
+         :weapon "Longbow"
+         :score 100}
+   :jane {:class "Knight"
+          :weapon "Greatsword"
+          :score 140}
+   :ryan {:class "Wizard"
+          :weapon "Mystic Staff"
+          :score 150}})
+
+(let [{{:keys [class weapon]} :joe} multiplayer-game-state]
+  (println "Joe is a" class "wielding a" weapon))
--- src/clojure_bytes/functions.clj
+++ src/clojure_bytes/functions.clj
@@ -1,4 +1,71 @@
 (ns clojure-bytes.functions)
 
+;; a function with one argument
 (defn square [x]
   (* x x))
+
+;; a private function. a private function can be called only within the namespace
+(defn- cube [x]
+  (* x x x))
+
+(defn get-cube [x]
+  (cube x))
+
+;; a function with two argument
+(defn multiply [x y]
+  (* x y))
+
+;; a function with n argument
+(defn multiply-n [& xs]
+  (reduce * xs))
+
+(defn x-and-many-y [x & y]
+  (println x)
+  (println y))
+
+;; function with optional parameter
+(defn string->integer [str & [base]]
+  (Integer/parseInt str (or base 10)))
+
+(defn string->integer-with-params [str {:keys [base] :or {base 10}}]
+  (Integer/parseInt str (or base 10)))
+
+;; a conditional function based on arguments
+(defn messenger
+  ([] (println "Welcome to clojure!"))
+  ([msg] (println msg)))
+
+(defn messenger-p
+  ([] (messenger-p "Hello world!"))
+  ([msg] (println msg)))
+
+;; aynonymous functions
+;; Equivalent to: (fn [x] (+ 6 x))
+#(+ 6 %)
+(#(+ 6 %) 7)
+(#(* %1 %2) 5 6)
+
+;; JAVA INTEROP
+; (.instanceMember instance args*)
+; (.instanceMember Classname args*)
+; (.-instanceField instance)
+; (Classname/staticMethod args*)
+; Classname/staticField
+
+;; instance method
+(.length "hello")
+(.toUpperCase "hello")
+
+;; instance field
+(.-x (java.awt.Point. 1 2))
+
+;; static method
+(Math/sqrt 25)
+
+;; static field
+(System/currentTimeMillis)
+
+;; Instantiation
+(def foo (java.util.HashMap.))
+(-> foo (.put :a "c"))
+(-> foo (.put :b "d"))
